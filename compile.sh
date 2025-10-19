132bb6cc501e3999fc3af81f46265598e7d49db1
ys452188 <minhuachen167@gmail.com>
2025-10-19 11:29:25 +0800

    在写存储驱动

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-echo "编译引导扇区(boot.asm)"
-nasm -f bin boot.asm -o boot.bin     #编译boot.asm
+#echo "编译引导扇区(boot.asm)"
+#nasm -f bin boot.asm -o boot.bin     #编译boot.asm
 #编译内核
 echo "编译内核(kernel.c)"
 clang -target aarch64-none-elf -ffreestanding -c kernel.c -o kernel.o
@@ -8,6 +8,6 @@ clang -target aarch64-none-elf -c kernel_entry.s -o kernel_entry.o
 echo "链接"
 ld.lld -T link.ld -o MyOS.elf kernel_entry.o kernel.o
 #删除编译时产生的其他文件
-rm kernel_entry.o kernel.o boot.bin
+rm kernel_entry.o kernel.o #boot.bin
 llvm-objcopy -O binary MyOS.elf MyOS.img
 echo "编译完成,镜像为 MyOS.img"
--- kernel.c
+++ kernel.c
@@ -1,14 +1,18 @@
 //串口输出
+#include "Disk/StorageDriver.h"
 #define UART0 0x101f1000
 static volatile unsigned int * const UART0DR = (unsigned int *)UART0;
-void urt0_log(const char *s) {
+//串口输出
+void uart0_log(const char *s) {
     while (*s) {
-        *UART0DR = (unsigned int)(*s++);
+    	//等TX空了再写入字符
+        if((UART0DR[5] & (1 << 5))) *UART0DR = (unsigned int)(*s++);
     }
 }
 //内核的入口函数
 void kmain(void) {
-	urt0_log("Hello, MyOS!");
+	//uart0_log("Hello, MyOS!");
+	//initDisk();
     //如果不加这一行，函数结束后 CPU 会继续执行内存中未知的指令，很快崩溃或重启。  
     while (1); // 死循环防止退出
 }
